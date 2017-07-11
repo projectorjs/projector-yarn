d4a767b9a8e666a2da3b4fc36ae3fc6be5046505
James Kyle <me@thejameskyle.com>
2017-07-12 00:43:16 +1000

    Use projector-spawn

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 // @flow
 'use strict';
 
-const bufferedSpawn = require('buffered-spawn');
+const spawn = require('projector-spawn');
 
 /*::
 type RunOptions = {
   name: string,
   flags?: Array<string>,
+  cwd?: string,
 };
 */
 
 exports.run = (opts /*: RunOptions */) => {
   let args = ['run', opts.name, '-s', '--'];
-  if (opts.flags) args = args.concat(opts.flags);
-  return bufferedSpawn('yarn', args);
+
+  if (opts.flags) {
+    args = args.concat(opts.flags);
+  }
+
+  return spawn('yarn', args, {
+    cwd: opts.cwd,
+  });
 };
